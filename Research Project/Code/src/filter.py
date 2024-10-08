d53ae8bc2d036491fb200992fb941af3bd1eb9a8
Tj-Alder-ANU <u7287129@anu.edu.au>
2024-10-08 12:33:49 +1100

    Generate torque curve

--- Research Project/Code/src/filter.py
+++ Research Project/Code/src/filter.py
@@ -8,3 +8,9 @@ def rolling_avg(x: np.ndarray, N: int=4, mode='same') -> np.ndarray:
     filter = np.ones(N)/N
     y = convolve(x,filter,mode=mode)
     return y
+
+def normalise(x: np.ndarray) -> np.ndarray:
+    x = x.astype('float')
+    x -= x.min()
+    x /= x.max()
+    return x
