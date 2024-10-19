abfa6270809493c4b74a8a5f1db37067956433df
Tj-Alder-ANU <u7287129@anu.edu.au>
2024-10-19 17:41:41 +1100

    Finalised filtered trip data

--- Research Project/Code/src/filter.py
+++ Research Project/Code/src/filter.py
@@ -1,4 +1,5 @@
 from scipy.signal import convolve
+from scipy.ndimage import gaussian_filter1d
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,6 +9,9 @@ def rolling_avg(x: np.ndarray, N: int=4, mode='same') -> np.ndarray:
     filter = np.ones(N)/N
     y = convolve(x,filter,mode=mode)
     return y
+
+def gaussian(x: np.ndarray, N: int=7, sigma: int=1, mode: str='constant') -> np.ndarray:
+    return gaussian_filter1d(x, sigma=sigma, radius=N//2, mode=mode)
 
 def normalise(x: np.ndarray) -> np.ndarray:
     x = x.astype('float')
